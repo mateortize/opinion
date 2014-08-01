d1e89dbc9610839a9e0e1cce99f0fc8a7d9ef1c8
xing <xingxing2d@gmail.com>
2014-08-01 21:09:12 +0800

    layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_account!
+  skip_before_action :verify_authenticity_token
+
   def impress
+  end
+
+  def contact
   end
 end
