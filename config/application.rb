3184bd1a23280b109dba2f517dd8b9a7ceb4b8c0
xing <xingxing2d@gmail.com>
2014-08-05 21:04:56 +0800

    locales

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Opinion7
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :de, :it]
 
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
