353c1e816b00b19e84fd207e625a83af5f022abe
xing <xingxing2d@gmail.com>
2014-08-08 21:45:30 +0800

    flags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def country_flag(code, label=true)
+    if label == true
+      return "<span class=\'flag #{code}\'>#{I18nData.countries[code.upcase.to_s]}</span>".html_safe
+    else
+      return "<span class=\'flag #{code}\'>&nbsp;</span>".html_safe
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module Opinion7
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     
     config.i18n.default_locale = :en
-    config.i18n.fallbacks =[:en]
-    config.i18n.available_locales = [:en, :de, :fr, :it, :pt, :zh, :es, :ja, :ru, :ko, :ar, :nl, :tr, :tw]
+    config.i18n.fallbacks =[:us]
+    config.i18n.available_locales = [:gb, :us, :cn, :es, :jp, :de, :fr, :pt, :ru, :it, :kr, :tw, :nl, :tr]
 
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
