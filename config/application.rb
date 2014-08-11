d917f05a714266591a5bad8b08bfebbee76a018a
xing <xingxing2d@gmail.com>
2014-08-11 17:18:35 +0800

    changed default locale

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Opinion7
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :de
     config.i18n.fallbacks =[:us]
     config.i18n.available_locales = [:gb, :us, :cn, :es, :jp, :de, :fr, :pt, :ru, :it, :kr, :tw, :nl, :tr]
 
