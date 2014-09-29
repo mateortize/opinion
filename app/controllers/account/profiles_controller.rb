e0764bbb1db458f8851a3754ac3d9911f52e911d
xing <xingxing2d@gmail.com>
2014-09-29 22:15:19 +0800

    added account profile edit form

---
+++ app/controllers/account/profiles_controller.rb
@@ -0,0 +1,21 @@
+class Account::ProfilesController < Account::BaseController
+
+  def edit
+    @account = current_account
+  end
+
+  def update
+    if current_account.update_attributes(account_params)
+      redirect_to account_surveys_path, notice: "Account information was updated successfully."
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def account_params
+    params.fetch(:account, {})
+          .permit(:first_name, :last_name, :avatar_image)
+  end
+end
