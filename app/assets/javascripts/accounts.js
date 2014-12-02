8919e6a6f80a5dd455a648821de15e9f89e63340
Marco Simon <m.simon@bonofa.com>
2014-12-02 13:26:36 +0100

    Complete rebuild (NOT yet finished!) – implementation of google translator

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -6,3 +6,12 @@ $(".fileupload").fileinput({
   showUpload: false,
   browseClass: "btn btn-success"
 });
+
+function check_translator() {
+  if ($('body').children('.skiptranslate:first-child').css('display') == 'block')
+    $('.navbar-fixed-top').addClass('translator');
+  else
+    $('.navbar-fixed-top').removeClass('translator');
+}
+
+window.setInterval('check_translator()', 100);
